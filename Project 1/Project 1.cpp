37e31c5bc477c8092b7ea51df6747fe1513a8083
IIaMoCLitTMW <szulc.tobiasz@gmail.com>
2021-06-25 13:50:03 +0200

    Add project files.

---
+++ Project 1/Project 1.cpp
@@ -0,0 +1,14 @@
+﻿// Project 1.cpp : Defines the entry point for the application.
+//
+
+#include "Project 1.h"
+
+#include "version_number_test.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << VERSION_NUMBER_TEST_MAJOR << " " << VERSION_NUMBER_TEST_MINOR << endl;
+	return 0;
+}
